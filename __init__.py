c2e6472e668a8ea3f8f1ccbf6462de9cbc37e04f
Gokul Sudheesh <38152409+gostar2000@users.noreply.github.com>
2020-07-18 23:05:36 +0400

    Delete __init__.py

--- __init__.py
+++
@@ -1,6 +0,0 @@
-from Gaussiandistribution import Gaussian
-from Binomialdistribution import Binomial
-'''
-Add '.' in fron of the parent package name if any error pops up
-from .Gaussiandistribution import Gaussian
-from .Binomialdistribution import Binomial'''
