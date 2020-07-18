5616623ca2baf600c97e9fdcab48c7dfb1c41fdb
Gokul Sudheesh <38152409+gostar2000@users.noreply.github.com>
2020-07-18 23:05:47 +0400

    Delete example.py

--- example.py
+++
@@ -1,6 +0,0 @@
-from Gaussiandistribution import Gaussian
-Gaussianobj1 = Gaussian(2,3)
-Gaussianobj2 = Gaussian(4,5)
-Gaussianobj3 = Gaussian()
-Gaussianobj3 = Gaussianobj1+Gaussianobj2
-print(Gaussianobj3.mean)
