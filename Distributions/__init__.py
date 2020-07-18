316fca207dd7b08c781b77d75706840a0c0df44c
Gokul Sudheesh <38152409+gostar2000@users.noreply.github.com>
2020-07-18 23:09:40 +0400

    Add files via upload

---
+++ Distributions/__init__.py
@@ -0,0 +1,6 @@
+from .Gaussiandistribution import Gaussian
+from .Binomialdistribution import Binomial
+'''
+Add '.' in fron of the parent package name if any error pops up
+from .Gaussiandistribution import Gaussian
+from .Binomialdistribution import Binomial'''
