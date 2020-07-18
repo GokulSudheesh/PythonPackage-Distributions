d7e72ed1f19995d78a8ee6066d588e9faed8301d
Gokul Sudheesh <38152409+gostar2000@users.noreply.github.com>
2020-07-18 23:10:18 +0400

    Add files via upload

---
+++ example.py
@@ -0,0 +1,6 @@
+from Distributions.Gaussiandistribution import Gaussian
+Gaussianobj1 = Gaussian(2,3)
+Gaussianobj2 = Gaussian(4,5)
+Gaussianobj3 = Gaussian()
+Gaussianobj3 = Gaussianobj1+Gaussianobj2
+print(Gaussianobj3.mean)
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      zip_safe=False)
+'''pip will automatically look for this file. 
+The set up file contains meta data about the package.'''
